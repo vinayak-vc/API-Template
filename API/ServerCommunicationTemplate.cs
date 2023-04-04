a8156e9cbbfbcbd5b4b385f53b2800e0243089e6
Vinayak Lakhani <vinayak.lakhani@viitor.cloud>
2023-04-04 17:52:40 +0530

    init

---
+++ API/ServerCommunicationTemplate.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+using ViitorCloud.API.StandardTemplates;
+
+namespace ViitorCloud.API {
+    public class ServerCommunicationTemplate : PersistentLazySingleton<ServerCommunicationTemplate> {
+        #region [API]
+        /// <summary>
+        /// This method call server API to get Login.
+        /// </summary>
+        /// <param name="callbackOnSuccess">Callback on success.</param>
+        /// <param name="callbackOnFail">Callback on fail.</param>
+        public void RequestLogin(string form, UnityAction<APIResponse<LoginResponse>> callbackOnSuccess,
+            UnityAction<string> callbackOnFail) {
+            ServerCommunication.Instance.SendRequestPost(form, Constants.API.Login,
+                callbackOnSuccess, callbackOnFail);
+        }
+
+        /// <summary>
+        /// This method call server API to get Login.
+        /// </summary>
+        /// <param name="callbackOnSuccess">Callback on success.</param>
+        /// <param name="callbackOnFail">Callback on fail.</param>
+        public void RequestRegister(string form, UnityAction<APIResponse<LoginResponse>> callbackOnSuccess,
+            UnityAction<string> callbackOnFail) {
+            ServerCommunication.Instance.SendRequestPost(form, Constants.API.Register,
+                callbackOnSuccess, callbackOnFail);
+        }
+     
+        #endregion [API]          
+    }
+}
