9dfa7c4999d5c90489ebfd660ccd752026e1a930
Vinayak Lakhani <47971927+vinayak-vc@users.noreply.github.com>
2025-03-05 13:18:55 +0530

    Update Constants.cs

--- API/Constants.cs
+++ API/Constants.cs
@@ -8,6 +8,9 @@ namespace ViitorCloud.API.Constants {
     }
 
     public class API {
+        
+        public static AppConfig appConfig;
+
         public static string APIDevelopmentBaseURL = "https://stg-demourl.com/";
         public static string APIProductionBaseURL = "https://prod-demourl.com/";
 
@@ -21,10 +24,43 @@ namespace ViitorCloud.API.Constants {
                         return APIProductionBaseURL;
                     case Server.Development:
                         return APIDevelopmentBaseURL;
+                    case Server.FromConfig:
+                        return appConfig.APIBaseURL;
                 }
 
                 return APIProductionBaseURL;
             }
+        }
+        
+        public class AppConfig {
+            public bool offline;
+            public string APIBaseURL;
+        }
+        
+        public static void LoadFromConfig() {
+            appConfig = null;
+            string _configPath = UnityEngine.Application.dataPath + "/config.json";
+
+            try {
+                if (System.IO.File.Exists(_configPath)) {
+                    appConfig = UnityEngine.JsonUtility.FromJson<AppConfig>(System.IO.File.ReadAllText(_configPath));
+                } else {
+                    GenerateJSON();
+                }
+            } catch (System.Exception e) {
+                appConfig = new AppConfig();
+                appConfig.APIBaseURL = APIProductionBaseURL;
+                appConfig.offline = false;
+                UnityEngine.Debug.LogError(e.Message);
+            }
+        }
+        private static void GenerateJSON() {
+            string _configPath = UnityEngine.Application.dataPath + "/config.json";
+            AppConfig appConfig = new AppConfig();
+            appConfig.APIBaseURL = APIProductionBaseURL;
+            appConfig.offline = false;
+           System.IO. File.WriteAllText(_configPath, UnityEngine.JsonUtility.ToJson(appConfig, true));
+            appConfig = null;
         }
 
         public enum Server {
